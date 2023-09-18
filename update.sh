ce11dd20c42e38047f1e2d4e6350051f4bd5822c
etjec4 <144890343+etjec4@users.noreply.github.com>
2023-09-18 09:30:45 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 echo "Memos一键更新脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/memos-on-replit"
-rm -rf memos && wget -O memos https://cautiousredundantnginx.nb486.repl.co/main
+rm -rf memos && wget -O memos https://github.com/usememos/memos/suites/16232260034/artifacts/927213661
 echo "更新至v0.11.2版本，更新完毕！"
 echo "点击Run按钮重新启动本项目！！！"
